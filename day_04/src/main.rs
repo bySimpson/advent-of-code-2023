4c580e8782588f1c1f23822314109558c909e651
Thamer Simon <thamersimon@gmail.com>
2023-12-04 18:29:04 +0100

    day_04 part_02

--- day_04/src/main.rs
+++ day_04/src/main.rs
@@ -3,7 +3,6 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 use clap::Parser;
 use rayon::prelude::*;
-use num_traits::Pow;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -37,6 +36,16 @@ impl Scratchcard {
             *c_item
         }).collect::<Vec<u32>>()
     }
+
+    pub fn calculate_points(&self) -> u32 {
+        let count = self.find_winning_numbers().par_iter().count() as u32;
+        if count == 0 {
+            0
+        }
+        else {
+            2_u32.pow(count - 1)
+        }
+    }
 }
 
 fn parse_numbers(input: &str) -> Vec<u32> {
@@ -50,7 +59,8 @@ fn main() -> Result<(), Box<dyn Error>> {
     let file = File::open(args.path)?;
     let reader = BufReader::new(file);
     let input = reader.lines().map(|c_item| c_item.unwrap()).collect::<Vec<String>>();
-    let mut game: Vec<Scratchcard> = input.par_iter().map(|c_line| {
+    // Cannot use rayon because order is important!
+    let game: Vec<Scratchcard> = input.iter().map(|c_line| {
         let (card_id_str, game_input) = c_line.split_once(":").unwrap();
         let card_id = card_id_str.split_whitespace().last().unwrap().parse::<u32>().unwrap();
         let mut number_iterator = game_input.split("|");
@@ -59,16 +69,24 @@ fn main() -> Result<(), Box<dyn Error>> {
         Scratchcard::new(card_id, winning_numbers, owned_numbers)
     }).collect();
 
-    let part_1: u32 = game.into_par_iter().map(|c_scratchcard| {
-        println!("{:#?}", c_scratchcard.find_winning_numbers());
-        let count = c_scratchcard.find_winning_numbers().iter().count() as u32;
-        if count == 0 {
-            0
-        }
-        else {
-            Pow::pow(2_u32, count - 1)
-        }
+    let part_1: u32 = game.par_iter().map(|c_scratchcard| {
+        c_scratchcard.calculate_points()
     }).sum::<u32>();
-    println!("{:#?}", part_1);
+
+    // hold amount of cards; SHIFTED BY -1!
+    let mut amount_of_cards_lookup: Vec<u32> = vec![1; input.len()];
+
+    println!("Part 1:\t{:#?}", part_1);
+
+    // Cannot use rayon because order is important!
+    game.iter().for_each(|c_scratchcard| {
+        let c_points = c_scratchcard.find_winning_numbers().iter().count();
+        //let c_amount_lookup_id = c_scratchcard.id -1;
+        let c_amount_lookup = *amount_of_cards_lookup.get(c_scratchcard.id as usize - 1).unwrap();
+        let _ = &amount_of_cards_lookup[c_scratchcard.id as usize..c_scratchcard.id as usize+c_points].par_iter_mut().for_each(|c_number| {
+            *c_number += c_amount_lookup;
+        });
+    });
+    println!("Part 2:\t{:?}", amount_of_cards_lookup.par_iter().sum::<u32>());
     Ok(())
 }
