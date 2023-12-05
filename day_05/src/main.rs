a7bdd1c4616444371c7c6e61f06203f92bbc4301
Thamer Simon <thamersimon@gmail.com>
2023-12-05 20:30:32 +0100

    day_05 part_02 brute force

--- day_05/src/main.rs
+++ day_05/src/main.rs
@@ -2,6 +2,7 @@
 use std::error::Error;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use std::ops::{Range, RangeInclusive};
 use clap::Parser;
 use rayon::prelude::*;
 
@@ -58,6 +59,7 @@ impl GenericMap {
 #[derive(Debug, Clone)]
 struct Game {
     seeds: Vec<u64>,
+    seeds_part_2: Vec<RangeInclusive<u64>>,
     maps: Vec<GenericMap>
 }
 
@@ -65,13 +67,27 @@ impl Game {
     pub fn new() -> Self {
         Self {
             seeds: vec![],
-            maps: vec![]
+            maps: vec![],
+            seeds_part_2: vec![]
         }
     }
 
     pub fn set_seeds(&mut self, line: &str) {
         let iterator = line.split_whitespace();
         self.seeds = iterator.skip(1).map(|c_line| c_line.parse::<u64>().unwrap()).collect()
+    }
+
+    pub fn set_seeds_part_2(&mut self, line: &str) {
+        let mut iterator = line.split_whitespace().skip(1);
+        let amount_to_check = iterator.clone().count() / 2;
+        let mut c_counter = 0;
+
+        while c_counter < amount_to_check  {
+            let start_val = iterator.next().unwrap().parse::<u64>().unwrap();
+            let end_val = start_val + iterator.next().unwrap().parse::<u64>().unwrap();
+            self.seeds_part_2.push(start_val..=end_val);
+            c_counter += 1;
+        }
     }
 
     pub fn insert_map(&mut self, genreric_map: GenericMap) {
@@ -97,16 +113,29 @@ impl Game {
         }
     }
 
+    pub fn traverse(&self, c_seed: u64) -> u64 {
+        let mut c_location_map = 0;
+        let mut c_nmbr = c_seed;
+        while c_location_map <= GenericMapType::HumidityToLocation as i64 {
+            c_nmbr = self.find_destination(c_location_map, c_nmbr);
+            c_location_map += 1;
+        }
+        c_nmbr
+    }
+
     pub fn play_part_1(&self) -> u64 {
         assert_ne!(self.seeds.len(), 0);
         self.seeds.par_iter().map(|c_seed| {
-            let mut c_location_map = 0;
-            let mut c_nmbr = *c_seed;
-            while c_location_map <= GenericMapType::HumidityToLocation as i64 {
-                c_nmbr = self.find_destination(c_location_map, c_nmbr);
-                c_location_map += 1;
-            }
-            c_nmbr
+            self.traverse(*c_seed)
+        }).min().unwrap()
+    }
+
+    pub fn play_part_2(&self) -> u64 {
+
+        self.seeds_part_2.clone().into_par_iter().map(|c_seed_range| {
+            c_seed_range.par_bridge().map(|c_seed| {
+                self.traverse(c_seed)
+            }).min().unwrap()
         }).min().unwrap()
     }
 }
@@ -144,6 +173,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         if empty_lines_counter == -1 {
             // seeds!
             game.set_seeds(c_line.as_str());
+            game.set_seeds_part_2(c_line.as_str());
         } else if empty_lines_counter >= 0 {
             if c_line.split_whitespace().count() == 3 {
                 // ignore first line describing map type!
@@ -154,6 +184,8 @@ fn main() -> Result<(), Box<dyn Error>> {
     // also insert last one!
     game.insert_map(c_generic_map);
     println!("Part 1:\t{}", game.play_part_1());
+    println!("Part 2:\t{}", game.play_part_2());
+
 
     Ok(())
 }
