d36b6377fbf80153e4b5c70c57e4c2b0e96144a2
Thamer Simon <thamersimon@gmail.com>
2023-12-07 21:01:26 +0100

    day_07 part_02

--- day_07/src/main.rs
+++ day_07/src/main.rs
@@ -3,6 +3,7 @@ use std::cmp::Ordering::{Equal, Greater, Less};
 use std::collections::HashMap;
 use std::error::Error;
 use std::fs::File;
+use std::hint;
 use std::io::{BufRead, BufReader};
 use clap::Parser;
 use rayon::prelude::*;
@@ -19,6 +20,7 @@ struct Args {
 #[derive(Debug, Clone, Copy, PartialOrd, PartialEq, Hash, Eq, Ord)]
 #[repr(u64)]
 enum Card {
+    Joker,
     Number(u64),
     Ten,
     Jack,
@@ -26,6 +28,13 @@ enum Card {
     King,
     Ace,
 }
+
+/*// ugly but the easiest way to give Jack the lowered Value!
+#[derive(Debug, Clone, Copy, PartialOrd, PartialEq, Hash, Eq, Ord)]
+#[repr(u64)]
+enum CardPart2 {
+
+}*/
 
 impl Card {
     pub fn parse(input: char) -> Self {
@@ -82,7 +91,7 @@ struct Hand {
     cards: Vec<Card>,
     card_count: HashMap<Card, u64>,
     bid: u64,
-    highest_value: Value
+    highest_value: Value,
 }
 
 impl Hand {
@@ -118,7 +127,7 @@ impl Hand {
             cards,
             bid,
             card_count,
-            highest_value: Value::HighCard
+            highest_value: Value::HighCard,
         };
         out.highest_value = out.get_best_hand();
         out
@@ -158,6 +167,55 @@ impl Hand {
             }
         }
 
+    }
+
+    pub fn update_best_card_to_part_2(&mut self) {
+        // replace Jack with Jokers!!! Jack should also not be in Counter!
+        let amount_of_jokers = *self.card_count.get(&Card::Jack).unwrap();
+        self.card_count.insert(Card::Jack, 0);
+        self.cards.par_iter_mut().for_each(|c_card| {
+            if *c_card == Card::Jack {
+                *c_card = Card::Joker
+            }
+        });
+
+        let max_amount_of_cards = *self.card_count.values().max().unwrap();
+        let highest = match max_amount_of_cards + amount_of_jokers {
+            5 => {
+                Value::FifeOfAKind
+            }
+            4 => {
+                Value::FourOfAKind
+            }
+            3 => {
+                let amount_of_2 = self.card_count.values().par_bridge().filter(|c_count| **c_count == 2).count();
+                let amount_of_3 = self.card_count.values().par_bridge().filter(|c_count| **c_count == 3).count();
+
+                if (amount_of_2 >= 1 && amount_of_3 >= 1) ||
+                    (amount_of_2 == 2 && amount_of_jokers >= 1) {
+                    Value::FullHouse
+                } else {
+                    Value::ThreeOfAKind
+                }
+            }
+            2 => {
+                // Two Pairs?!
+                let pairs_without_joker = self.card_count.values().par_bridge().filter(|c_count| **c_count == 2).count();
+                if pairs_without_joker == 2 || (amount_of_jokers == 1 && pairs_without_joker == 1) {
+                    // Two pairs
+                    Value::TwoPair
+                } else {
+                    Value::OnePair
+                }
+            }
+            1 => {
+                Value::HighCard
+            }
+            n => {
+                panic!("Undefined amount of matches, not possible: {}", n)
+            }
+        };
+        self.highest_value = highest;
     }
 }
 
@@ -228,5 +286,21 @@ fn main() -> Result<(), Box<dyn Error>> {
     }).sum::<u64>();
 
     println!("Part 1:\t{}", part_01);
+
+    game.iter_mut().for_each(|c_card| {
+       c_card.update_best_card_to_part_2();
+    });
+
+    game.sort();
+
+    let mut counter = 1;
+    let part_02 = game.iter().map(|c_hand| {
+        let out = counter * c_hand.bid;
+        counter += 1;
+        out
+    }).sum::<u64>();
+
+    println!("Part 2:\t{}", part_02);
+
     Ok(())
 }
