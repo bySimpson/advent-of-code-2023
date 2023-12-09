a5302255b800697b5b257cdf2ab0b1ef7347a0fd
Thamer Simon <thamersimon@gmail.com>
2023-12-09 15:15:51 +0100

    day_09 part_01

---
+++ day_09/src/main.rs
@@ -0,0 +1,55 @@
+use std::error::Error;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use clap::Parser;
+use rayon::prelude::*;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    path: String,
+    #[arg(short, long)]
+    debug: bool
+}
+
+fn is_leading_zeroes(in_vec: &Vec<i64>) -> bool {
+    in_vec.par_iter().all(|c_nmbr| *c_nmbr == 0)
+}
+
+fn get_differences(in_vec: &Vec<i64>) -> Vec<i64> {
+    in_vec.windows(2).map(|c_vals| {
+        c_vals[1] - c_vals[0]
+    }).collect()
+}
+
+fn extrapolate_part_01(in_vec: &Vec<i64>) -> i64 {
+    if is_leading_zeroes(in_vec) {
+        return 0
+    }
+    let diff_to_next_val = extrapolate_part_01(&get_differences(in_vec));
+    in_vec.last().unwrap() + diff_to_next_val
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let args = Args::parse();
+    let file = File::open(args.path)?;
+    let reader = BufReader::new(file);
+    let input = reader.lines().map(|c_item| c_item.unwrap()).collect::<Vec<String>>();
+
+    let mut values: Vec<Vec<i64>> = input.iter().map(|c_line| {
+        let mut c_vec: Vec<i64> = vec![];
+        c_line.split_whitespace().for_each(|c_item| {
+            c_vec.push(c_item.parse::<i64>().unwrap());
+        });
+        c_vec
+    }).collect();
+
+    let part_01 = values.iter().fold(0, |mut acc, c_values| {
+        acc += extrapolate_part_01(c_values);
+        acc
+    });
+
+    println!("Part 1:\t{:?}", part_01);
+    Ok(())
+}
